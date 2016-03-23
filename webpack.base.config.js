abfdbe0025852bb1722b86db19d0f3f769aa6b8a
杨佳军 <yangjj@chezhil.com>
2016-03-23 14:35:27 +0800

    no message

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -4,7 +4,7 @@ var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
 
-  devtool: 'source-map',
+  // devtool: 'source-map',
 
   entry: {
     demo:"./demo/main",
--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -3,7 +3,6 @@ var config = require('./webpack.base.config')
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-config.devtool = 'source-map'
 
 config.output.filename = '[name].[chunkhash:8].js'
 config.output.chunkFilename = '[id].[chunkhash:8].js'
@@ -12,16 +11,16 @@ config.output.chunkFilename = '[id].[chunkhash:8].js'
 config.plugins = (config.plugins || []).concat([
   //把所有独立样式的CSS打包成一个style.css
   new ExtractTextPlugin("styles.[hash:8].css",{allChunks: true}),
-  new webpack.optimize.CommonsChunkPlugin({
-    name: "vendor",
-
-    filename: "[name].[hash:8].js",
-    // (Give the chunk a different name)
-
-    minChunks: Infinity,
-    // (with more entries, this ensures that no other module
-    //  goes into the vendor chunk)
-  }),
+  // new webpack.optimize.CommonsChunkPlugin({
+  //   name: "vendor",
+  //
+  //   filename: "[name].[hash:8].js",
+  //   // (Give the chunk a different name)
+  //
+  //   minChunks: Infinity,
+  //   // (with more entries, this ensures that no other module
+  //   //  goes into the vendor chunk)
+  // }),
   new webpack.optimize.UglifyJsPlugin({
     compressor: {
       warnings: false
@@ -32,7 +31,7 @@ config.plugins = (config.plugins || []).concat([
     template: './server/views/dist/layout.html',
     filename: 'layout.html',
     inject: 'body',
-    chunks: ['vendor', 'web_app'],
+    chunks: ['demo'],
     minify:{    //压缩HTML文件
       removeComments:true,    //移除HTML中的注释
       collapseWhitespace:false    //删除空白符与换行符
